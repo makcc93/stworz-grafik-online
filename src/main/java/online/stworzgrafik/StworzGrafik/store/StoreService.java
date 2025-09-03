91b951a4c27cc7b809b0bd6d35021da08a95d975
Mateusz Kruk <makoo93@gmail.com>
2025-09-03 18:34:30 +0200

    Add new methods

--- src/main/java/online/stworzgrafik/StworzGrafik/store/StoreService.java
+++ src/main/java/online/stworzgrafik/StworzGrafik/store/StoreService.java
@@ -5,7 +5,11 @@ import online.stworzgrafik.StworzGrafik.store.DTO.ResponseStoreDTO;
 import online.stworzgrafik.StworzGrafik.store.DTO.StoreNameAndCodeDTO;
 import online.stworzgrafik.StworzGrafik.store.DTO.UpdateStoreDTO;
 
+import java.util.List;
+
 public interface StoreService {
+    List<ResponseStoreDTO> findAll();
+    ResponseStoreDTO findById(Long id);
     ResponseStoreDTO create(CreateStoreDTO createStoreDTO);
     ResponseStoreDTO update(Long id, UpdateStoreDTO updateStoreDTO);
     boolean exists(Long id);
