c4d7b7fe436bdd0bdd6908e8fb866de282f68435
Mateusz Kruk <makoo93@gmail.com>
2025-09-03 18:33:52 +0200

    Add controller

---
+++ src/main/java/online/stworzgrafik/StworzGrafik/store/controller/StoreController.java
@@ -0,0 +1,37 @@
+package online.stworzgrafik.StworzGrafik.store.controller;
+
+import jakarta.validation.Valid;
+import online.stworzgrafik.StworzGrafik.store.DTO.CreateStoreDTO;
+import online.stworzgrafik.StworzGrafik.store.DTO.ResponseStoreDTO;
+import online.stworzgrafik.StworzGrafik.store.StoreServiceImpl;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/stores")
+public class StoreController {
+    private StoreServiceImpl service;
+
+
+    public StoreController(StoreServiceImpl service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ResponseStoreDTO>> getAllStores(){
+        return ResponseEntity.ok(service.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ResponseStoreDTO> getStoreById(@PathVariable Long id){
+        return ResponseEntity.ok(service.findById(id));
+    }
+
+    @PostMapping()
+    public ResponseEntity<ResponseStoreDTO> createStore(@RequestBody @Valid CreateStoreDTO createStoreDTO){
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.create(createStoreDTO));
+    }
+}
