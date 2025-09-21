6b172593a96a24460984ab5eb65aafbdcde961fc
Mateusz Kruk <makoo93@gmail.com>
2025-09-21 23:24:10 +0200

    Add new class for handle name in one place

---
+++ src/test/java/online/stworzgrafik/StworzGrafik/branch/validator/NameValidatorTest.java
@@ -0,0 +1,47 @@
+package online.stworzgrafik.StworzGrafik.branch.validator;
+
+
+import jakarta.validation.ValidationException;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class NameValidatorTest {
+
+    @Test
+    void validate_workingTest(){
+        //given
+        String name = "Test6";
+
+        //when
+        String validated = NameValidator.validate(name);
+
+        //then
+        assertEquals("TEST6",validated);
+    }
+
+    @Test
+    void validate_illegalCharThrowsException() {
+        //given
+        String name = "Test!@#$%^&*()";
+
+        //when
+        ValidationException validationException = assertThrows(jakarta.validation.ValidationException.class, () -> NameValidator.validate(name));
+
+        //then
+        assertEquals("Branch name cannot contains illegal chars", validationException.getMessage());
+    }
+
+    @Test
+    void validate_deletingWhiteSpaces(){
+        //given
+        String name = "       I \n L O V E    \t\n\t\n programming";
+
+        //when
+        String validated = NameValidator.validate(name);
+
+        //then
+        assertEquals("ILOVEPROGRAMMING", validated);
+    }
+}
