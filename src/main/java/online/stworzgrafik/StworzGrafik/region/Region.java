a2bf24183323a1f97cadd258053128ba98c03286
Mateusz Kruk <makoo93@gmail.com>
2025-09-29 22:26:42 +0200

    Add new entity

---
+++ src/main/java/online/stworzgrafik/StworzGrafik/region/Region.java
@@ -0,0 +1,34 @@
+package online.stworzgrafik.StworzGrafik.region;
+
+import jakarta.persistence.*;
+import lombok.*;
+import online.stworzgrafik.StworzGrafik.branch.Branch;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Region {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private boolean isEnable;
+
+    @OneToMany(mappedBy = "region")
+    private List<Branch> branches = new ArrayList<>();
+
+    @PrePersist
+    void onCreate(){
+        isEnable = true;
+    }
+
+}
