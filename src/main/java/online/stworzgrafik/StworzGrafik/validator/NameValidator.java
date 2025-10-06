16533b4e447b6ee637f6479889ef0ba4dec30553
Mateusz Kruk <makoo93@gmail.com>
2025-10-06 23:51:46 +0200

    Add new name validatior method

--- src/main/java/online/stworzgrafik/StworzGrafik/validator/NameValidator.java
+++ src/main/java/online/stworzgrafik/StworzGrafik/validator/NameValidator.java
@@ -15,4 +15,14 @@ public final class NameValidator {
 
         return name.strip().replaceAll("\\s+","").toUpperCase();
     }
+
+    public static String validateForPosition(String name){
+        ArgumentNullChecker.check(name, "Name");
+
+        if (!name.matches("^[a-zA-ZąćęłńóśźżĄĘĆŁŃÓŚŹŻ\\t\\n ]+$")){
+            throw new ValidationException("Name cannot contains illegal chars");
+        }
+
+        return name.trim().toUpperCase();
+    }
 }
