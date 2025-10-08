8c5fece6c32c92874c3b79f0256a34257096ed88
Mateusz Kruk <makoo93@gmail.com>
2025-10-08 22:30:08 +0200

    Refactorization

--- src/main/java/online/stworzgrafik/StworzGrafik/employee/position/PositionBuilder.java
+++ src/main/java/online/stworzgrafik/StworzGrafik/employee/position/PositionBuilder.java
@@ -1,19 +1,20 @@
 package online.stworzgrafik.StworzGrafik.employee.position;
 
+import jakarta.annotation.Nullable;
 import online.stworzgrafik.StworzGrafik.exception.ArgumentNullChecker;
 import online.stworzgrafik.StworzGrafik.validator.NameValidator;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PositionBuilder {
-    public Position createPostion(String name){
+    public Position createPosition(String name, @Nullable String description){
         ArgumentNullChecker.check(name,"Name");
 
         String validated = NameValidator.validateForPosition(name);
 
         return Position.builder()
                 .name(validated)
+                .description(description)
                 .build();
     }
-    //test it and then go for mapper and then for service(Impl)
 }
