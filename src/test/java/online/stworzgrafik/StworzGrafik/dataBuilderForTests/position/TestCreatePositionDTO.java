22f94ae416038fc1a870acfea713bc8a1e8b2941
Mateusz Kruk <makoo93@gmail.com>
2025-10-09 10:36:22 +0200

    Add default data for tests only

---
+++ src/test/java/online/stworzgrafik/StworzGrafik/dataBuilderForTests/position/TestCreatePositionDTO.java
@@ -0,0 +1,26 @@
+package online.stworzgrafik.StworzGrafik.dataBuilderForTests.position;
+
+import online.stworzgrafik.StworzGrafik.employee.position.DTO.CreatePositionDTO;
+
+public class TestCreatePositionDTO {
+    private String name = "TESTCREATENAME";
+    private String description = "TESTCREATEDESCRIPTION";
+
+    public TestCreatePositionDTO withName(String name){
+        this.name = name;
+        return this;
+    }
+
+    public TestCreatePositionDTO withDescription(String description){
+        this.description = description;
+        return this;
+    }
+
+    public CreatePositionDTO build(){
+        return new CreatePositionDTO(
+                name,
+                description
+        );
+    }
+
+}
