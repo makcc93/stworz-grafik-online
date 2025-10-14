5c4d3f760768ca7dd4fc6ee6a6ca90d9b52bb39a
Mateusz Kruk <makoo93@gmail.com>
2025-10-14 23:43:11 +0200

    Add new DTOs

--- src/main/java/online/stworzgrafik/StworzGrafik/shift/DTO/ResponseShiftDTO.java
+++ src/main/java/online/stworzgrafik/StworzGrafik/shift/DTO/ResponseShiftDTO.java
@@ -1,10 +1,12 @@
 package online.stworzgrafik.StworzGrafik.shift.DTO;
 
+import jakarta.validation.constraints.NotNull;
+
 import java.time.LocalTime;
 
 public record ResponseShiftDTO(
-    Long id,
-    LocalTime startHour,
-    LocalTime endHour,
-    Integer length
+    @NotNull Long id,
+    @NotNull LocalTime startHour,
+    @NotNull LocalTime endHour,
+    @NotNull Integer length
 ){}
--- src/main/java/online/stworzgrafik/StworzGrafik/shift/DTO/ShiftHoursDTO.java
+++ src/main/java/online/stworzgrafik/StworzGrafik/shift/DTO/ShiftHoursDTO.java
@@ -6,9 +6,6 @@ import jakarta.validation.constraints.NotNull;
 import java.time.LocalTime;
 
 public record ShiftHoursDTO (
-        @NotNull
-        LocalTime startHour,
-
-        @NotNull
-        LocalTime endHour
+        @NotNull LocalTime startHour,
+        @NotNull LocalTime endHour
 ){}
