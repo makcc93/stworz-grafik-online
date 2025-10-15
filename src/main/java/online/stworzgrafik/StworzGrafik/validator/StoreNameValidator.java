a84897d596f4e5f4abe916c43790db4585af2e90
Mateusz Kruk <makoo93@gmail.com>
2025-10-15 23:43:46 +0200

    Update validator

--- src/main/java/online/stworzgrafik/StworzGrafik/validator/StoreNameValidator.java
+++ src/main/java/online/stworzgrafik/StworzGrafik/validator/StoreNameValidator.java
@@ -1,7 +1,9 @@
 package online.stworzgrafik.StworzGrafik.validator;
 
 import jakarta.validation.ValidationException;
+import org.springframework.stereotype.Service;
 
+@Service
 public class StoreNameValidator implements NameValidatorStrategy{
     @Override
     public String validate(String name) {
