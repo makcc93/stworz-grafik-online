f0aac1e7b405ff18afae326c0a9bf84adc8e0155
Mateusz Kruk <makoo93@gmail.com>
2025-10-16 22:14:36 +0200

    Add new validator for position

---
+++ src/main/java/online/stworzgrafik/StworzGrafik/validator/PositionNameValidator.java
@@ -0,0 +1,21 @@
+package online.stworzgrafik.StworzGrafik.validator;
+
+import jakarta.validation.ValidationException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PositionNameValidator implements NameValidatorStrategy{
+    @Override
+    public String validate(String name) {
+        if (!name.matches("^[a-zA-ZąćęłńóśźżĄĘĆŁŃÓŚŹŻ -]+$")){
+            throw new ValidationException("Name cannot contains illegal chars");
+        }
+
+        return name.strip().toUpperCase();
+    }
+
+    @Override
+    public ObjectType getSupportedType() {
+        return ObjectType.POSITION;
+    }
+}
