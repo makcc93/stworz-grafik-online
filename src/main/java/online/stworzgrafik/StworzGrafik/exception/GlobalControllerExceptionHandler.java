d643b0ef4159db1f23b49d41e7276402033440fb
Mateusz Kruk <makoo93@gmail.com>
2025-10-17 23:38:41 +0200

    Add request body exception handler

--- src/main/java/online/stworzgrafik/StworzGrafik/exception/GlobalControllerExceptionHandler.java
+++ src/main/java/online/stworzgrafik/StworzGrafik/exception/GlobalControllerExceptionHandler.java
@@ -1,20 +1,26 @@
 package online.stworzgrafik.StworzGrafik.exception;
 
+import jakarta.persistence.ElementCollection;
 import jakarta.persistence.EntityExistsException;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.ValidationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @ControllerAdvice
 public class GlobalControllerExceptionHandler {
 
     @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity<Void> entityNotFound(EntityNotFoundException e){
-        return ResponseEntity.notFound().build();
+    public ResponseEntity<String> entityNotFound(EntityNotFoundException e){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
@@ -35,6 +41,20 @@ public class GlobalControllerExceptionHandler {
     @ExceptionHandler(ValidationException.class)
     public ResponseEntity<String> nameContainsIllegalChars(ValidationException e){
         return ResponseEntity.badRequest().body("Name contains illegal char(s)");
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String,Object>> methodValidationException(MethodArgumentNotValidException exception){
+        List<String> errors = exception.getBindingResult().getFieldErrors().stream()
+                .map(error -> error.getField() + ": " + error.getDefaultMessage())
+                .toList();
+
+
+        Map<String,Object> response = new HashMap<>();
+        response.put("status",exception.getStatusCode().value());
+        response.put("error", errors);
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
     }
 
     @ExceptionHandler(Exception.class)
