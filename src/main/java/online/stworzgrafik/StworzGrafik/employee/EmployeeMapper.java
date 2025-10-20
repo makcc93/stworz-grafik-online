b10163e10cffffef7eb79847f2c84b586aad75d4
Mateusz Kruk <makoo93@gmail.com>
2025-10-21 00:15:24 +0200

    Add mapper

---
+++ src/main/java/online/stworzgrafik/StworzGrafik/employee/EmployeeMapper.java
@@ -0,0 +1,21 @@
+package online.stworzgrafik.StworzGrafik.employee;
+
+import online.stworzgrafik.StworzGrafik.employee.DTO.ResponseEmployeeDTO;
+import online.stworzgrafik.StworzGrafik.employee.DTO.UpdateEmployeeDTO;
+import org.mapstruct.*;
+
+@Mapper
+public interface EmployeeMapper {
+
+
+    @Mapping(source = "store.id" ,target = "storeId")
+    @Mapping(source = "position.id", target = "positionId")
+    ResponseEmployeeDTO toResponseEmployeeDTO(Employee employee);
+
+    @Mapping(source = "storeId", target = "store.id")
+    @Mapping(source = "positionId", target = "position.id")
+    Employee toEmployee(ResponseEmployeeDTO responseEmployeeDTO);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updateEmployee(UpdateEmployeeDTO updateEmployeeDTO, @MappingTarget Employee employee);
+}
