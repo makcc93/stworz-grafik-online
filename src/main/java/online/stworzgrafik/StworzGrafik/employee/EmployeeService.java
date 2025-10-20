ea2621e07ba3ab10442063a19c519815a7c89a9b
Mateusz Kruk <makoo93@gmail.com>
2025-10-21 00:17:09 +0200

    Add interface service with methods to implement

---
+++ src/main/java/online/stworzgrafik/StworzGrafik/employee/EmployeeService.java
@@ -0,0 +1,23 @@
+package online.stworzgrafik.StworzGrafik.employee;
+
+import jakarta.validation.constraints.NotNull;
+import online.stworzgrafik.StworzGrafik.employee.DTO.CreateEmployeeDTO;
+import online.stworzgrafik.StworzGrafik.employee.DTO.ResponseEmployeeDTO;
+import online.stworzgrafik.StworzGrafik.employee.DTO.UpdateEmployeeDTO;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+
+import java.util.List;
+
+@Validated
+@Service
+public interface EmployeeService {
+    ResponseEmployeeDTO createEmployee(@NotNull CreateEmployeeDTO createEmployeeDTO);
+    ResponseEmployeeDTO update(@NotNull UpdateEmployeeDTO updateEmployeeDTO);
+    void deleteEmployee(@NotNull Long id);
+    List<ResponseEmployeeDTO> findAll();
+    ResponseEmployeeDTO findById(@NotNull Long id);
+    boolean existsById(@NotNull Long id);
+    boolean existsBySap(@NotNull Long sap);
+    boolean existsByLastName(@NotNull String lastName);
+}
