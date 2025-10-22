16d09fe5fe32b3726d927211f9050e92f52cca76
Mateusz Kruk <makoo93@gmail.com>
2025-10-22 23:32:50 +0200

    Add integrated tests

---
+++ src/test/java/online/stworzgrafik/StworzGrafik/employee/EmployeeServiceImplIT.java
@@ -0,0 +1,178 @@
+package online.stworzgrafik.StworzGrafik.employee;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.persistence.EntityExistsException;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
+import jakarta.validation.ValidationException;
+import online.stworzgrafik.StworzGrafik.branch.Branch;
+import online.stworzgrafik.StworzGrafik.branch.BranchRepository;
+import online.stworzgrafik.StworzGrafik.dataBuilderForTests.branch.TestBranchBuilder;
+import online.stworzgrafik.StworzGrafik.dataBuilderForTests.employee.TestCreateEmployeeDTO;
+import online.stworzgrafik.StworzGrafik.dataBuilderForTests.employee.TestEmployeeBuilder;
+import online.stworzgrafik.StworzGrafik.dataBuilderForTests.position.TestPositionBuilder;
+import online.stworzgrafik.StworzGrafik.dataBuilderForTests.region.TestRegionBuilder;
+import online.stworzgrafik.StworzGrafik.dataBuilderForTests.store.TestStoreBuilder;
+import online.stworzgrafik.StworzGrafik.employee.DTO.CreateEmployeeDTO;
+import online.stworzgrafik.StworzGrafik.employee.DTO.ResponseEmployeeDTO;
+import online.stworzgrafik.StworzGrafik.employee.position.Position;
+import online.stworzgrafik.StworzGrafik.employee.position.PositionRepository;
+import online.stworzgrafik.StworzGrafik.region.Region;
+import online.stworzgrafik.StworzGrafik.region.RegionRepository;
+import online.stworzgrafik.StworzGrafik.store.Store;
+import online.stworzgrafik.StworzGrafik.store.StoreRepository;
+import online.stworzgrafik.StworzGrafik.validator.NameValidatorService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+public class EmployeeServiceImplIT {
+    @Autowired
+    private EmployeeService employeeService;
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Autowired
+    private EmployeeMapper employeeMapper;
+
+    @Autowired
+    private NameValidatorService nameValidatorService;
+
+    @Autowired
+    private StoreRepository storeRepository;
+
+    @Autowired
+    private PositionRepository positionRepository;
+
+    @Autowired
+    private EmployeeBuilder employeeBuilder;
+
+    @Autowired
+    private BranchRepository branchRepository;
+
+    @Autowired
+    private RegionRepository regionRepository;
+
+    @Test
+    void createEmployee_workingTest(){
+        //given
+        String firstName = "JOHN";
+        String lastName = "SILVERHAND";
+        Long sap = 2077L;
+        Store defaultSavedStore = getDefaultSavedStore();
+        Position defaultSavedPosition = getDefaultSavedPosition();
+
+        CreateEmployeeDTO createEmployeeDTO = new TestCreateEmployeeDTO()
+                .withFirstName(firstName)
+                .withLastName(lastName)
+                .withSap(sap)
+                .withStoreId(defaultSavedStore.getId())
+                .withPositionId(defaultSavedPosition.getId())
+                .build();
+
+
+        //when
+        ResponseEmployeeDTO serviceResponse = employeeService.createEmployee(createEmployeeDTO);
+
+        //then
+        assertEquals(firstName,serviceResponse.firstName());
+        assertEquals(lastName,serviceResponse.lastName());
+        assertEquals(sap,serviceResponse.sap());
+
+        assertTrue(employeeRepository.existsBySap(sap));
+    }
+
+    @Test
+    void createEmployee_employeeWithThisSapAlreadyExistsThrowsException(){
+        //given
+        Store store = getDefaultSavedStore();
+        Position position = getDefaultSavedPosition();
+
+        Employee employee = new TestEmployeeBuilder().withStore(store).withPosition(position).build();
+        employeeRepository.save(employee);
+
+        Long employeeSap = employee.getSap();
+
+        CreateEmployeeDTO createEmployeeDTO = new TestCreateEmployeeDTO().withSap(employeeSap).build();
+
+        //when
+        EntityExistsException exception =
+                assertThrows(EntityExistsException.class, () -> employeeService.createEmployee(createEmployeeDTO));
+
+        //then
+        assertEquals("Employee with sap " + employeeSap + " already exists", exception.getMessage());
+        assertTrue(employeeRepository.existsBySap(employeeSap));
+    }
+
+    @Test
+    void createEmployee_namesValidationThrowsException(){
+        //given
+        String invalidFirstName = "WRONG FIRST NAME!@#$%^&*()";
+        CreateEmployeeDTO invalidFirstNameDTO = new TestCreateEmployeeDTO().withFirstName(invalidFirstName).build();
+
+        String invalidLastName = "WRONG LAST NAME!@$%^&*()";
+        CreateEmployeeDTO invalidLastNameDTO = new TestCreateEmployeeDTO().withLastName(invalidLastName).build();
+
+        //when
+        ValidationException exceptionFistName = assertThrows(ValidationException.class, () -> employeeService.createEmployee(invalidFirstNameDTO));
+        ValidationException exceptionLastName = assertThrows(ValidationException.class, () -> employeeService.createEmployee(invalidLastNameDTO));
+
+        //then
+        assertEquals("Name cannot contains illegal chars",exceptionFistName.getMessage());
+        assertEquals(exceptionFistName.getMessage(), exceptionLastName.getMessage());
+    }
+
+    @Test
+    void createEmployee_sapNumberLengthIsIncorrectThrowsException(){
+        //given
+        Store store = getDefaultSavedStore();
+        Position position = getDefaultSavedPosition();
+
+        Long randomPositionId = 12345L;
+        CreateEmployeeDTO withoutPositionDTO = new TestCreateEmployeeDTO()
+                .withPositionId(randomPositionId)
+                .withStoreId(store.getId())
+                .build();
+
+        Long randomStoreId = 54321L;
+        CreateEmployeeDTO withoutStoreDTO = new TestCreateEmployeeDTO()
+                .withStoreId(randomStoreId)
+                .withPositionId(position.getId())
+                .build();
+
+        //when
+        EntityNotFoundException exceptionForPosition =
+                assertThrows(EntityNotFoundException.class, () -> employeeService.createEmployee(withoutPositionDTO));
+
+        EntityNotFoundException exceptionForStore =
+                assertThrows(EntityNotFoundException.class, () -> employeeService.createEmployee(withoutStoreDTO));
+
+        //then
+        assertEquals("Cannot find position by id " + randomPositionId, exceptionForPosition.getMessage());
+        assertEquals("Cannot find store by id " + randomStoreId, exceptionForStore.getMessage());
+    }
+
+    private Store getDefaultSavedStore(){
+        Region region = new TestRegionBuilder().build();
+        regionRepository.save(region);
+
+        Branch branch = new TestBranchBuilder().withRegion(region).build();
+        branchRepository.save(branch);
+
+
+        Store store = new TestStoreBuilder().withBranch(branch).build();
+        return storeRepository.save(store);
+    }
+
+    private Position getDefaultSavedPosition(){
+        Position position = new TestPositionBuilder().build();
+        return positionRepository.save(position);
+    }
+}
