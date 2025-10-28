c2575495bdedb9176c0d37a5ff42576b4e2d8e99
Mateusz Kruk <makoo93@gmail.com>
2025-10-28 10:49:58 +0100

    Refactorization

--- src/main/java/online/stworzgrafik/StworzGrafik/employee/EmployeeRepository.java
+++ src/main/java/online/stworzgrafik/StworzGrafik/employee/EmployeeRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 public interface EmployeeRepository extends JpaRepository<Employee,Long>, JpaSpecificationExecutor<Employee> {
     boolean existsBySap(Long sap);
+    boolean existsByLastName(String lastName);
 }
