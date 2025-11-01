ef0d8ae2e2738743628d2887be299da5d975d456
Mateusz Kruk <makoo93@gmail.com>
2025-11-01 22:42:09 +0100

    Add controller

---
+++ src/main/java/online/stworzgrafik/StworzGrafik/employee/controller/EmployeeController.java
@@ -0,0 +1,27 @@
+package online.stworzgrafik.StworzGrafik.employee.controller;
+
+import jakarta.validation.Valid;
+import online.stworzgrafik.StworzGrafik.employee.DTO.CreateEmployeeDTO;
+import online.stworzgrafik.StworzGrafik.employee.DTO.ResponseEmployeeDTO;
+import online.stworzgrafik.StworzGrafik.employee.EmployeeService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/employees")
+public class EmployeeController {
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @PostMapping
+    public ResponseEntity<ResponseEmployeeDTO> createEmployee (@Valid @RequestBody CreateEmployeeDTO createEmployeeDTO){
+        return ResponseEntity.status(HttpStatus.CREATED).body(employeeService.createEmployee(createEmployeeDTO));
+    }
+}
