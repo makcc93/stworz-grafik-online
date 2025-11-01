685705f79a36c02cc2d7f67bbb39d32be5b973c1
Mateusz Kruk <makoo93@gmail.com>
2025-11-01 23:50:56 +0100

    Add new endpoints

--- src/main/java/online/stworzgrafik/StworzGrafik/employee/controller/EmployeeController.java
+++ src/main/java/online/stworzgrafik/StworzGrafik/employee/controller/EmployeeController.java
@@ -1,15 +1,16 @@
 package online.stworzgrafik.StworzGrafik.employee.controller;
 
+import jakarta.validation.ReportAsSingleViolation;
 import jakarta.validation.Valid;
 import online.stworzgrafik.StworzGrafik.employee.DTO.CreateEmployeeDTO;
 import online.stworzgrafik.StworzGrafik.employee.DTO.ResponseEmployeeDTO;
+import online.stworzgrafik.StworzGrafik.employee.DTO.UpdateEmployeeDTO;
 import online.stworzgrafik.StworzGrafik.employee.EmployeeService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/employees")
@@ -20,8 +21,25 @@ public class EmployeeController {
         this.employeeService = employeeService;
     }
 
+    @GetMapping
+    public ResponseEntity<List<ResponseEmployeeDTO>> findAll(){
+        return ResponseEntity.ok(employeeService.findAll());
+    }
+
+    @GetMapping("/{employeeId}")
+    public ResponseEntity<ResponseEmployeeDTO> findById(@Valid @PathVariable Long employeeId){
+        return ResponseEntity.ok(employeeService.findById(employeeId));
+    }
+
     @PostMapping
     public ResponseEntity<ResponseEmployeeDTO> createEmployee (@Valid @RequestBody CreateEmployeeDTO createEmployeeDTO){
         return ResponseEntity.status(HttpStatus.CREATED).body(employeeService.createEmployee(createEmployeeDTO));
     }
+
+    @PatchMapping("/{employeeId}")
+    public ResponseEntity<ResponseEmployeeDTO> updateEmployee(@Valid @PathVariable Long employeeId,@Valid @RequestBody UpdateEmployeeDTO updateEmployeeDTO){
+        return ResponseEntity.ok(employeeService.updateEmployee(employeeId,updateEmployeeDTO));
+    }
+
+    //time for DeleteMapping and tests
 }
