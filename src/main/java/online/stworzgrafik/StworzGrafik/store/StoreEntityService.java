26595fd0b680f70b37ac551ef3f3cf1187ddfc31
Mateusz Kruk <makoo93@gmail.com>
2025-11-16 22:45:29 +0100

    Refactorization

--- src/main/java/online/stworzgrafik/StworzGrafik/store/StoreEntityService.java
+++ src/main/java/online/stworzgrafik/StworzGrafik/store/StoreEntityService.java
@@ -1,10 +1,11 @@
 package online.stworzgrafik.StworzGrafik.store;
 
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.validation.annotation.Validated;
 
 @Validated
 public interface StoreEntityService {
-    public Store saveEntity(@NotNull Store store);
-    public Store getEntityById(@NotNull Long id);
+    Store saveEntity(@NotNull @Valid Store store);
+    Store getEntityById(@NotNull Long id);
 }
--- src/main/java/online/stworzgrafik/StworzGrafik/store/StoreService.java
+++ src/main/java/online/stworzgrafik/StworzGrafik/store/StoreService.java
@@ -12,12 +12,12 @@ import java.util.List;
 
 @Validated
 public interface StoreService {
-    public List<ResponseStoreDTO> findAll();
-    public ResponseStoreDTO findById(@NotNull Long id);
-    public ResponseStoreDTO createStore(@NotNull @Valid CreateStoreDTO createStoreDTO);
-    public ResponseStoreDTO update(@NotNull Long id, @NotNull @Valid UpdateStoreDTO updateStoreDTO);
-    public boolean exists(@NotNull Long id);
-    public boolean exists(@NotNull @Valid StoreNameAndCodeDTO storeNameAndCodeDTO);
-    public void delete(@NotNull Long id);
-    public ResponseStoreDTO save(@Valid Store store);
+    List<ResponseStoreDTO> findAll();
+    ResponseStoreDTO findById(@NotNull Long id);
+    ResponseStoreDTO createStore(@NotNull @Valid CreateStoreDTO createStoreDTO);
+    ResponseStoreDTO update(@NotNull Long id,@NotNull @Valid UpdateStoreDTO updateStoreDTO);
+    boolean existsById(@NotNull Long id);
+    boolean existsByNameAndCode(@NotNull @Valid StoreNameAndCodeDTO storeNameAndCodeDTO);
+    void delete(@NotNull Long id);
+    ResponseStoreDTO save(@NotNull @Valid Store store);
 }
