1f6447c0cdc7dc629ea9f18d91ad0670444feed1
Mateusz Kruk <makoo93@gmail.com>
2025-12-06 23:22:49 +0100

    New DTO

---
+++ src/main/java/online/stworzgrafik/StworzGrafik/draft/DTO/StoreAccurateDayDemandDraftDTO.java
@@ -0,0 +1,25 @@
+package online.stworzgrafik.StworzGrafik.draft.DTO;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
+public record StoreAccurateDayDemandDraftDTO(
+        @NotNull Long storeId,
+
+        @NotNull
+        @Min(1900)
+        @Max(2100)
+        Integer year,
+
+        @NotNull
+        @Min(1)
+        @Max(12)
+        Integer month,
+
+        @NotNull
+        @Min(1)
+        @Max(31)
+        Integer day
+) {
+}
