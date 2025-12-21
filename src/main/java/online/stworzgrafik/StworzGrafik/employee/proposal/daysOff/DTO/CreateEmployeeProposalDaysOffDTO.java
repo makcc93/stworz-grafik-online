8e613d1725673ba3ab0a81eec5dcd9d65732c150
Mateusz Kruk <makoo93@gmail.com>
2025-12-22 00:03:07 +0100

    Init dto

---
+++ src/main/java/online/stworzgrafik/StworzGrafik/employee/proposal/daysOff/DTO/CreateEmployeeProposalDaysOffDTO.java
@@ -0,0 +1,25 @@
+package online.stworzgrafik.StworzGrafik.employee.proposal.daysOff.DTO;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import online.stworzgrafik.StworzGrafik.employee.Employee;
+import online.stworzgrafik.StworzGrafik.store.Store;
+
+public record CreateEmployeeProposalDaysOffDTO(
+        @NotNull
+        @Min(value = 2000,message = "Year minimum value is 2000")
+        @Max(value = 2099, message = "Year maximum value is 2099")
+        Integer year,
+
+        @NotNull
+        @Min(value = 1,message = "Month minimum value is 1")
+        @Max(value = 12, message = "Month maximum value is 12")
+        Integer month,
+
+        @NotNull
+        @Size(min=31, max = 31, message = "Employee monthly proposal days off array must have exactly 31 elements")
+        int[] monthlyDaysOff
+        ) {
+}
