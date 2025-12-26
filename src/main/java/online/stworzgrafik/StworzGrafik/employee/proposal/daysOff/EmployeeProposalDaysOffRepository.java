3875380ad478c30014745638bf79ab9e1fd2a5b8
Mateusz Kruk <makoo93@gmail.com>
2025-12-26 23:06:36 +0100

    Add methods to repo

--- src/main/java/online/stworzgrafik/StworzGrafik/employee/proposal/daysOff/EmployeeProposalDaysOffRepository.java
+++ src/main/java/online/stworzgrafik/StworzGrafik/employee/proposal/daysOff/EmployeeProposalDaysOffRepository.java
@@ -1,9 +1,11 @@
 package online.stworzgrafik.StworzGrafik.employee.proposal.daysOff;
 
-import jakarta.persistence.criteria.CriteriaBuilder;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.util.Optional;
+
 interface EmployeeProposalDaysOffRepository extends JpaRepository<EmployeeProposalDaysOff,Long>, JpaSpecificationExecutor<EmployeeProposalDaysOff> {
     boolean existsByStoreIdAndEmployeeIdAndYearAndMonth(Long storeId, Long employeeId, Integer year, Integer month);
+    Optional<EmployeeProposalDaysOff> findByStoreIdAndEmployeeIdAndYearAndMonth(Long storeId, Long employeeId, Integer year, Integer month);
 }
