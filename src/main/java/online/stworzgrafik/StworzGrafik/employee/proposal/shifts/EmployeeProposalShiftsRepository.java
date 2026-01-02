6767ac6d1d4606fe61d2009503abae765c0406a6
Mateusz Kruk <makoo93@gmail.com>
2026-01-02 23:38:58 +0100

    Refactorization

--- src/main/java/online/stworzgrafik/StworzGrafik/employee/proposal/shifts/EmployeeProposalShiftsRepository.java
+++ src/main/java/online/stworzgrafik/StworzGrafik/employee/proposal/shifts/EmployeeProposalShiftsRepository.java
@@ -3,5 +3,8 @@ package online.stworzgrafik.StworzGrafik.employee.proposal.shifts;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface EmployeeProposalShiftsRepository extends JpaRepository<EmployeeProposalShifts,Long>, JpaSpecificationExecutor<EmployeeProposalShifts> {
+import java.time.LocalDate;
+
+interface EmployeeProposalShiftsRepository extends JpaRepository<EmployeeProposalShifts,Long>, JpaSpecificationExecutor<EmployeeProposalShifts> {
+    boolean existsByStore_IdAndEmployee_IdAndDate(Long storeId, Long employeeId, LocalDate date);
 }
