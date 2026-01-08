d665394f3352dd139aba0a869b2421f25a80a6bf
Mateusz Kruk <makoo93@gmail.com>
2026-01-08 23:51:21 +0100

    Add controller

--- src/main/java/online/stworzgrafik/StworzGrafik/employee/proposal/shifts/controller/EmployeeProposalShiftsController.java
+++ src/main/java/online/stworzgrafik/StworzGrafik/employee/proposal/shifts/controller/EmployeeProposalShiftsController.java
@@ -2,9 +2,12 @@ package online.stworzgrafik.StworzGrafik.employee.proposal.shifts.controller;
 
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import online.stworzgrafik.StworzGrafik.employee.proposal.shifts.DTO.CreateEmployeeProposalShiftsDTO;
 import online.stworzgrafik.StworzGrafik.employee.proposal.shifts.DTO.ResponseEmployeeProposalShiftsDTO;
+import online.stworzgrafik.StworzGrafik.employee.proposal.shifts.DTO.UpdateEmployeeProposalShiftsDTO;
 import online.stworzgrafik.StworzGrafik.employee.proposal.shifts.EmployeeProposalShiftsService;
 import online.stworzgrafik.StworzGrafik.security.UserAuthorizationService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -21,7 +24,7 @@ public class EmployeeProposalShiftsController {
     private final UserAuthorizationService userAuthorizationService;
 
     @PreAuthorize("@userAuthorizationService.hasAccessToStore(#storeId)")
-    @GetMapping("/stores/{storeId}/employees/{employeeId}/proposalsShifts/{employeeProposalShiftId}")
+    @GetMapping("/stores/{storeId}/employees/{employeeId}/proposalShifts/{employeeProposalShiftId}")
     public ResponseEntity<ResponseEmployeeProposalShiftsDTO> getProposalShiftById(@PathVariable Long storeId,
                                                                                   @PathVariable Long employeeId,
                                                                                   @PathVariable Long employeeProposalShiftId){
@@ -29,12 +32,38 @@ public class EmployeeProposalShiftsController {
     }
 
     @PreAuthorize("@userAuthorizationService.hasAccessToStore(#storeId)")
-    @GetMapping("/stores/{storeId}/proposalsShifts")
+    @GetMapping("/stores/{storeId}/proposalShifts")
     public ResponseEntity<List<ResponseEmployeeProposalShiftsDTO>> getByCriteria(@PathVariable Long storeId,
                                                                                  @RequestParam(required = false) LocalDate startDate,
                                                                                  @RequestParam(required = false) LocalDate endDate,
-                                                                                 @RequestParam(required = false) Long employeeId){                                                                                ){
+                                                                                 @RequestParam(required = false) Long employeeId){
+        return ResponseEntity.ok(service.getByCriteria(storeId, startDate, endDate, employeeId));
+    }
 
-        return ResponseEntity.ok(service.getByCriteria(storeId,startDate,endDate,employeeId));
+    @PreAuthorize("@userAuthorizationService.hasAccessToStore(#storeId)")
+    @PutMapping("/stores/{storeId}/employees/{employeeId}/proposalShifts")
+    public ResponseEntity<ResponseEmployeeProposalShiftsDTO> createProposalShift(@PathVariable Long storeId,
+                                                                                 @PathVariable Long employeeId,
+                                                                                 CreateEmployeeProposalShiftsDTO dto){
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.createEmployeeProposalShift(storeId,employeeId,dto));
+    }
+
+    @PreAuthorize("@userAuthorizationService.hasAccessToStore(#storeId)")
+    @PatchMapping("/stores/{storeId}/employees/{employeeId}/proposalShifts/{employeeProposalShiftId}")
+    public ResponseEntity<ResponseEmployeeProposalShiftsDTO> updateProposalShift(@PathVariable Long storeId,
+                                                                                 @PathVariable Long employeeId,
+                                                                                 @PathVariable Long employeeProposalShiftId,
+                                                                                 @RequestBody UpdateEmployeeProposalShiftsDTO dto){
+        return ResponseEntity.ok(service.updateEmployeeProposalShift(storeId,employeeId,employeeProposalShiftId,dto));
+    }
+
+    @PreAuthorize("@userAuthorizationService.hasAccessToStore(#storeId)")
+    @DeleteMapping("/stores/{storeId}/employees/{employeeId}/proposalShifts/{employeeProposalShiftId}")
+    public ResponseEntity<HttpStatus> deleteProposalShift(@PathVariable Long storeId,
+                                                          @PathVariable Long employeeId,
+                                                          @PathVariable Long employeeProposalShiftId){
+        service.delete(storeId,employeeId,employeeProposalShiftId);
+
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
