0be4cd587e5f90a0ffd50ffe9791439e499bebdc
Mateusz Kruk <makoo93@gmail.com>
2026-01-08 23:54:20 +0100

    Delete unused methods

--- src/main/java/online/stworzgrafik/StworzGrafik/employee/proposal/shifts/EmployeeProposalShiftsRepository.java
+++ src/main/java/online/stworzgrafik/StworzGrafik/employee/proposal/shifts/EmployeeProposalShiftsRepository.java
@@ -1,19 +1,10 @@
 package online.stworzgrafik.StworzGrafik.employee.proposal.shifts;
 
-import online.stworzgrafik.StworzGrafik.employee.proposal.shifts.DTO.ResponseEmployeeProposalShiftsDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.time.LocalDate;
-import java.util.Collection;
-import java.util.List;
 
 interface EmployeeProposalShiftsRepository extends JpaRepository<EmployeeProposalShifts,Long>, JpaSpecificationExecutor<EmployeeProposalShifts> {
     boolean existsByStore_IdAndEmployee_IdAndDate(Long storeId, Long employeeId, LocalDate date);
-
-    List<EmployeeProposalShifts> findByStoreIdAndEmployeeId(Long storeId, Long employeeId);
-
-    List<EmployeeProposalShifts> findByStoreIdAndEmployeeIdAndDateBetween(Long storeId, LocalDate startDate, LocalDate endDate, Long employeeId);
-
-    List<EmployeeProposalShifts> findByStoreIdAndDateBetween(Long storeId, LocalDate startDate, LocalDate endDate);
 }
