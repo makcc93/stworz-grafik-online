3cb0b9234c074ba07591d6994f96a7165372fad2
Mateusz Kruk <makoo93@gmail.com>
2026-01-12 23:32:25 +0100

    Add vacation controller

---
+++ src/main/java/online/stworzgrafik/StworzGrafik/employee/vacation/controller/EmployeeVacationController.java
@@ -0,0 +1,68 @@
+package online.stworzgrafik.StworzGrafik.employee.vacation.controller;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import online.stworzgrafik.StworzGrafik.employee.vacation.DTO.CreateEmployeeVacationDTO;
+import online.stworzgrafik.StworzGrafik.employee.vacation.DTO.ResponseEmployeeVacationDTO;
+import online.stworzgrafik.StworzGrafik.employee.vacation.DTO.UpdateEmployeeVacationDTO;
+import online.stworzgrafik.StworzGrafik.employee.vacation.EmployeeVacationService;
+import online.stworzgrafik.StworzGrafik.security.UserAuthorizationService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@Transactional
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class EmployeeVacationController {
+    private final EmployeeVacationService service;
+    private final UserAuthorizationService userAuthorizationService;
+
+    @PreAuthorize("@userAuthorizationService.hasAccessToStore(#storeId)")
+    @GetMapping("/stores/{storeId}/employees/{employeeId}/vacations/{vacationId}")
+    public ResponseEntity<ResponseEmployeeVacationDTO> getVacationById(@PathVariable Long storeId,
+                                                                       @PathVariable Long employeeId,
+                                                                       @PathVariable Long vacationId){
+        return ResponseEntity.ok(service.getById(storeId,employeeId,vacationId));
+    }
+
+    @PreAuthorize("@userAuthorizationService.hasAccessToStore(#storeId)")
+    @GetMapping("/stores/{storeId}/vacations")
+    public ResponseEntity<List<ResponseEmployeeVacationDTO>> getByCriteria(@PathVariable Long storeId,
+                                                                          @RequestParam(required = false) Long employeeId,
+                                                                          @RequestParam(required = false) Integer year,
+                                                                          @RequestParam(required = false) Integer month){
+        return ResponseEntity.ok(service.getByCriteria(storeId,employeeId,year,month));
+    };
+
+    @PreAuthorize("@userAuthorizationService.hasAccessToStore(#storeId)")
+    @PutMapping("/stores/{storeId}/employees/{employeeId}/vacations")
+    public ResponseEntity<ResponseEmployeeVacationDTO> createVacation(@PathVariable Long storeId,
+                                                                     @PathVariable Long employeeId,
+                                                                     @RequestBody CreateEmployeeVacationDTO dto){
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.createEmployeeProposalVacation(storeId,employeeId,dto));
+    }
+
+    @PreAuthorize("@userAuthorizationService.hasAccessToStore(#storeId)")
+    @PatchMapping("/stores/{storeId}/employees/{employeeId}/vacations/{vacationId}")
+    public ResponseEntity<ResponseEmployeeVacationDTO> updateVacation(@PathVariable Long storeId,
+                                                                             @PathVariable Long employeeId,
+                                                                             @PathVariable Long vacationId,
+                                                                             @RequestBody UpdateEmployeeVacationDTO dto){
+        return ResponseEntity.ok(service.updateEmployeeVacation(storeId,employeeId,vacationId,dto));
+    }
+
+    @PreAuthorize("@userAuthorizationService.hasAccessToStore(#storeId)")
+    @DeleteMapping("/stores/{storeId}/employees/{employeeId}/vacations/{vacationId}")
+    public ResponseEntity<HttpStatus> deleteProposal(@PathVariable Long storeId,
+                                                     @PathVariable Long employeeId,
+                                                     @PathVariable Long vacationId){
+        service.delete(storeId,employeeId,vacationId);
+
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+}
