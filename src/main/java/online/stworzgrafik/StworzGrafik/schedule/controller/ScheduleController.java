49d4dea35a1072d2ce33e3b6e51a92eb4dd976e4
Mateusz Kruk <makoo93@gmail.com>
2026-02-06 22:42:25 +0100

    Add controller

---
+++ src/main/java/online/stworzgrafik/StworzGrafik/schedule/controller/ScheduleController.java
@@ -0,0 +1,59 @@
+package online.stworzgrafik.StworzGrafik.schedule.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import online.stworzgrafik.StworzGrafik.schedule.DTO.CreateScheduleDTO;
+import online.stworzgrafik.StworzGrafik.schedule.DTO.ResponseScheduleDTO;
+import online.stworzgrafik.StworzGrafik.schedule.DTO.ScheduleSpecificationDTO;
+import online.stworzgrafik.StworzGrafik.schedule.DTO.UpdateScheduleDTO;
+import online.stworzgrafik.StworzGrafik.schedule.ScheduleService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+class ScheduleController {
+    private final ScheduleService scheduleService;
+
+    @PreAuthorize("@userAuthorizationService.hasAccessToStore(#storeId)")
+    @GetMapping("/stores/{storeId}/schedules/{scheduleId}")
+    ResponseEntity<ResponseScheduleDTO> getById(@PathVariable Long storeId,
+                                                       @PathVariable Long scheduleId){
+        return ResponseEntity.ok(scheduleService.findById(storeId, scheduleId));
+    }
+
+    @GetMapping("/schedules")
+    ResponseEntity<Page<ResponseScheduleDTO>> getByCriteria(ScheduleSpecificationDTO dto,
+                                                            Pageable pageable){
+        return ResponseEntity.ok(scheduleService.findByCriteria(dto,pageable));
+    }
+
+    @PreAuthorize("@userAuthorizationService.hasAccessToStore(#storeId)")
+    @PostMapping("/stores/{storeId}/schedules")
+    ResponseEntity<ResponseScheduleDTO> createSchedule(@PathVariable Long storeId,
+                                                       @RequestBody @Valid CreateScheduleDTO dto){
+        return ResponseEntity.status(HttpStatus.CREATED).body(scheduleService.createSchedule(storeId,dto));
+    }
+
+    @PreAuthorize("@userAuthorizationService.hasAccessToStore(#storeId)")
+    @PatchMapping("/stores/{storeId}/schedules/{scheduleId}")
+    ResponseEntity<ResponseScheduleDTO> updateSchedule(@PathVariable Long storeId,
+                                                       @PathVariable Long scheduleId,
+                                                       @RequestBody UpdateScheduleDTO dto){
+        return ResponseEntity.ok(scheduleService.updateSchedule(storeId, scheduleId,dto));
+    }
+
+    @PreAuthorize("@userAuthorizationService.hasAccessToStore(#storeId)")
+    @DeleteMapping("/stores/{storeId}/schedules/{scheduleId}")
+    ResponseEntity<HttpStatus> deleteSchedule(@PathVariable Long storeId,
+                                              @PathVariable Long scheduleId){
+        scheduleService.deleteSchedule(storeId,scheduleId);
+
+        return ResponseEntity.noContent().build();
+    }
+}
