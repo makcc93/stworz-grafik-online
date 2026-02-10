6ae904683951e67f10363916dfad58309c948b94
Mateusz Kruk <makoo93@gmail.com>
2026-02-10 23:17:33 +0100

    Add string -> enum mapping

--- src/main/java/online/stworzgrafik/StworzGrafik/schedule/ScheduleMapper.java
+++ src/main/java/online/stworzgrafik/StworzGrafik/schedule/ScheduleMapper.java
@@ -7,11 +7,22 @@ import org.mapstruct.*;
 @Mapper(componentModel = "spring")
 interface ScheduleMapper {
 
+    default ScheduleStatus stringToEnum(String scheduleStatusName){
+        if (scheduleStatusName == null){
+            return null;
+        }
+
+        try {
+            return ScheduleStatus.valueOf(scheduleStatusName.toUpperCase());
+        }
+        catch (IllegalArgumentException exception){
+            throw new IllegalArgumentException("Unknown Schedule Status, " + exception);
+        }
+    }
+
     @Mapping(target = "storeId", source = "store.id")
     @Mapping(target = "scheduleStatusName", source = "scheduleStatus")
     ResponseScheduleDTO toDTO(Schedule schedule);
-
-//    Schedule toEntity();
 
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
     void updateSchedule(UpdateScheduleDTO dto, @MappingTarget Schedule schedule);
