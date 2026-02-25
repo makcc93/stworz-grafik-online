5d5c4f0c958795246630d6b897a2a9dea8e0147b
Mateusz Kruk <makoo93@gmail.com>
2026-02-25 23:53:00 +0100

    Montly schedule generator update

--- src/main/java/online/stworzgrafik/StworzGrafik/algorithm/MonthlyStoreScheduleGeneratorImpl.java
+++ src/main/java/online/stworzgrafik/StworzGrafik/algorithm/MonthlyStoreScheduleGeneratorImpl.java
@@ -5,43 +5,49 @@ import online.stworzgrafik.StworzGrafik.draft.DemandDraft;
 import online.stworzgrafik.StworzGrafik.draft.DemandDraftEntityService;
 import online.stworzgrafik.StworzGrafik.employee.Employee;
 import online.stworzgrafik.StworzGrafik.employee.EmployeeEntityService;
+import online.stworzgrafik.StworzGrafik.employee.proposal.daysOff.EmployeeProposalDaysOff;
+import online.stworzgrafik.StworzGrafik.employee.proposal.daysOff.EmployeeProposalDaysOffEntityService;
 import online.stworzgrafik.StworzGrafik.employee.proposal.shifts.EmployeeProposalShifts;
 import online.stworzgrafik.StworzGrafik.employee.proposal.shifts.EmployeeProposalShiftsEntityService;
+import online.stworzgrafik.StworzGrafik.employee.vacation.EmployeeVacation;
+import online.stworzgrafik.StworzGrafik.employee.vacation.EmployeeVacationEntityService;
+import online.stworzgrafik.StworzGrafik.employee.vacation.EmployeeVacationService;
 import online.stworzgrafik.StworzGrafik.schedule.Schedule;
 import online.stworzgrafik.StworzGrafik.schedule.ScheduleEntityService;
-import online.stworzgrafik.StworzGrafik.security.UserAuthorizationService;
-import online.stworzgrafik.StworzGrafik.shift.ShiftService;
 import online.stworzgrafik.StworzGrafik.store.Store;
 import online.stworzgrafik.StworzGrafik.store.StoreEntityService;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
 class MonthlyStoreScheduleGeneratorImpl implements MonthlyStoreScheduleGenerator{
-    private final UserAuthorizationService userAuthorizationService;
     private final StoreEntityService storeEntityService;
     private final EmployeeEntityService employeeEntityService;
     private final DemandDraftEntityService demandDraftEntityService;
     private final ScheduleEntityService scheduleEntityService;
     private final EmployeeProposalShiftsEntityService employeeProposalShiftsEntityService;
-    private final ShiftService shiftService;
+    private final EmployeeProposalDaysOffEntityService employeeProposalDaysOffEntityService;
+    private final EmployeeVacationEntityService employeeVacationEntityService;
 
     @Async
     @Override
     public void generateMonthlySchedule(Long storeId, Integer year, Integer month) {
         Schedule schedule = scheduleEntityService.findByStoreIdAndYearAndMonth(storeId,year,month);
-        final Store store = storeEntityService.getEntityById(userAuthorizationService.getUserStoreId());
-        final List<Employee> storeActiveEmployees = employeeEntityService.findAllStoreActiveEmployees(store.getId());
-        final Map<Integer, int[]> everyDayStoreDemandDraft = dayAndDemandDraft(storeId, year, month);
+        final Store store = storeEntityService.getEntityById(storeId);
+        final List<Employee> storeActiveEmployees = employeeEntityService.findAllStoreActiveEmployees(storeId);
+        final Map<LocalDate, int[]> everyDayStoreDemandDraft = dayAndDemandDraft(storeId, year, month);
+        final Map<LocalDate, Map<Employee, int[]>> monthlyEmployeesProposalShiftsByDate = employeeProposalShifts(storeId,year,month);
+        final Map<Employee, int[]> monthlyEmployeesProposalDayOffByMonth = employeeProposalDaysOff(storeId,year,month);
+        final Map<Employee, int[]> monthlyEmployeesVacationByMonth = monthlyEmployeesVacationMonth(storeId,year,month);
+
     }
 
-    private Map<Integer, int[]> dayAndDemandDraft(Long storeId, Integer year, Integer month) {
+    private Map<LocalDate, int[]> dayAndDemandDraft(Long storeId, Integer year, Integer month) {
         LocalDate firstDay = LocalDate.of(year,month,1);
         LocalDate lastDay = firstDay.withDayOfMonth(firstDay.lengthOfMonth());
 
@@ -49,26 +55,50 @@ class MonthlyStoreScheduleGeneratorImpl implements MonthlyStoreScheduleGenerator
 
         return monthlyDrafts.stream()
                 .collect(Collectors.toMap(
-                        draft -> draft.getDraftDate().getDayOfMonth(),
-                        DemandDraft::getHourlyDemand
+                            DemandDraft::getDraftDate,
+                            DemandDraft::getHourlyDemand,
+                            (e1, e2) -> {throw new IllegalStateException("Date in store draft cannot be duplicated");},
+                            TreeMap::new
                 ));
     }
 
-
-    //todo
-    //koncze na tym ze trzeba rozmninic teraz jak pobrac wszytskie propozycje i co z nimi zrobic
-    //czy juz teraz musze je miec dla kazdego pracownika czy potem bede sobie tylko filtrowal za dany dzien
-    private Map<Employee,int[]> employeeProposalShifts(Long storeId, List<Employee> employees, Integer year, Integer month){
+    private Map<LocalDate, Map<Employee, int[]>> employeeProposalShifts(Long storeId, Integer year, Integer month){
         LocalDate firstDay = LocalDate.of(year,month,1);
         LocalDate lastDay = firstDay.withDayOfMonth(firstDay.lengthOfMonth());
 
-        List<EmployeeProposalShifts> employeesProposalShifts = employeeProposalShiftsEntityService.findMonthlyStoreProposalShifts(storeId,firstDay,lastDay);
+        List<EmployeeProposalShifts> allMonthlyProposalShifts = employeeProposalShiftsEntityService.findMonthlyStoreProposalShifts(storeId,firstDay,lastDay);
 
-        return employeesProposalShifts.stream()
+        return allMonthlyProposalShifts.stream()
+                .collect(Collectors.groupingBy(
+                            EmployeeProposalShifts::getDate,
+                            TreeMap::new,
+                            Collectors.toMap(
+                                    EmployeeProposalShifts::getEmployee,
+                                    EmployeeProposalShifts::getDailyProposalShift
+                            )
+                        )
+                );
+    }
+
+    private Map<Employee,int[]> employeeProposalDaysOff(Long storeId, Integer year, Integer month){
+        List<EmployeeProposalDaysOff> employeesMonthlyProposalDayOff = employeeProposalDaysOffEntityService.getEmployeeMonthlyProposalDaysOff(storeId, year, month);
+
+        return employeesMonthlyProposalDayOff.stream()
                 .collect(Collectors.toMap(
-                        draft -> draft.getEmployee(),
-                        shiftService.getShiftAsArray(EmployeeProposalShifts::getDailyProposalShift)
-                ))
+                        EmployeeProposalDaysOff::getEmployee,
+                        EmployeeProposalDaysOff::getMonthlyDaysOff
+                        )
+                );
+    }
+
+    private Map<Employee, int[]> monthlyEmployeesVacationMonth(Long storeId, Integer year, Integer month){
+        List<EmployeeVacation> employeesMonthlyVacation = employeeVacationEntityService.getEmployeeMonthlyVacation(storeId,year,month);
+
+        return employeesMonthlyVacation.stream()
+                .collect(Collectors.toMap(
+                        EmployeeVacation::getEmployee,
+                        EmployeeVacation::getMonthlyVacation
+                ));
     }
 
 
