b3576b61bf243086911ef370d00e48fec92a89fa
Mateusz Kruk <makoo93@gmail.com>
2026-03-11 20:00:28 +0100

    Add shift generator for every day

---
+++ src/main/java/online/stworzgrafik/StworzGrafik/algorithm/DailyShiftGeneratorAlgorithm.java
@@ -0,0 +1,123 @@
+package online.stworzgrafik.StworzGrafik.algorithm;
+
+import de.jollyday.HolidayManager;
+import lombok.RequiredArgsConstructor;
+import online.stworzgrafik.StworzGrafik.draft.DemandDraftEntityService;
+import online.stworzgrafik.StworzGrafik.employee.Employee;
+import online.stworzgrafik.StworzGrafik.schedule.ScheduleEntityService;
+import online.stworzgrafik.StworzGrafik.schedule.details.ScheduleDetailsEntityService;
+import online.stworzgrafik.StworzGrafik.shift.Shift;
+import online.stworzgrafik.StworzGrafik.shift.ShiftEntityService;
+import online.stworzgrafik.StworzGrafik.shift.ShiftService;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.*;
+
+@Component
+@RequiredArgsConstructor
+class DailyShiftGeneratorAlgorithm {
+    private final ShiftService shiftService;
+    private final ShiftEntityService shiftEntityService;
+    private final ScheduleDetailsEntityService scheduleDetailsEntityService;
+    private final ScheduleEntityService scheduleEntityService;
+    private final DemandDraftEntityService dailyDemand;
+    private final HolidayManager holidayManager;
+
+    public void generateShiftsToDates(ScheduleGeneratorContext context) {
+        Map<LocalDate, int[]> everyDayStoreDemandDraft = context.getEveryDayStoreDemandDraftSorted();
+        List<Employee> employees = context.getStoreActiveEmployees();
+
+        for (Map.Entry<LocalDate, int[]> entry : everyDayStoreDemandDraft.entrySet()) {
+            int[] employeeDailyProposalCount = new int[24];
+            LocalDate date = entry.getKey();
+            int[] dateDraft = entry.getValue();
+
+            Map<LocalDate, Map<Employee, int[]>> monthlyEmployeesProposalShiftsByDate = context.getMonthlyEmployeesProposalShiftsByDate();
+            Map<Employee, int[]> dailyEmployeeProposals = monthlyEmployeesProposalShiftsByDate.getOrDefault(date, Collections.emptyMap());
+
+            for (Employee employee : employees) {
+                int[] employeeProposal = dailyEmployeeProposals.getOrDefault(employee, new int[24]);
+
+                employeeDailyProposalCount = addArrays(employeeDailyProposalCount,employeeProposal);
+            }
+
+            int[] draftAfterProposals = subtractArrays(dateDraft, employeeDailyProposalCount);
+
+            List<Shift> shifts = generateLowestPersonNeededDailyShifts(draftAfterProposals);
+
+            context.addShiftsToDate(date,shifts);
+        }
+    }
+
+    private List<Shift> generateLowestPersonNeededDailyShifts(int[] dailyDemandDraft) {
+        List<Shift> startHoursShifts = generateShiftStartHours(dailyDemandDraft);
+
+        List<Shift> shiftsSortedDesc = startHoursShifts.stream()
+                .sorted(Comparator.comparing(Shift::getStartHour).reversed())
+                .toList();
+
+        return generateShiftEndHours(shiftsSortedDesc, dailyDemandDraft);
+    }
+
+    private List<Shift> generateShiftEndHours(List<Shift> shiftsSortedDesc, int[] dailyDemandDraft) {
+        int index = 0;
+        for (int hourOfDay = 23; hourOfDay >= 0; hourOfDay--) {
+            int demand = dailyDemandDraft[hourOfDay];
+
+            if (demand > 0) {
+                int nextDemand = (hourOfDay == 23) ? 0 : dailyDemandDraft[hourOfDay + 1];
+                for (int i = demand; i > nextDemand; i--) {
+
+                    if (hourOfDay == 23){
+                        shiftsSortedDesc.get(index).setEndHour(LocalTime.of(0,0));
+                    } else {
+                        shiftsSortedDesc.get(index).setEndHour(LocalTime.of(hourOfDay + 1, 0));
+                    }
+                    index++;
+                }
+            }
+        }
+        return shiftsSortedDesc;
+    }
+
+    private List<Shift> generateShiftStartHours(int[] dailyDemandDraft) {
+        List<Shift> shifts = new ArrayList<>();
+
+        for (int hourOfDay = 0; hourOfDay < dailyDemandDraft.length; hourOfDay++) {
+            int demand = dailyDemandDraft[hourOfDay];
+
+            if (demand != 0) {
+                int previousDemand = (hourOfDay == 0) ? 0 : dailyDemandDraft[hourOfDay -1];
+                for (int i = demand; i > previousDemand; i--) {
+                    Shift shift = new Shift();
+                    shift.setStartHour(LocalTime.of(hourOfDay,0));
+
+                    shifts.add(shift);
+                }
+            }
+        }
+        return shifts;
+    }
+
+    private int[] addArrays(int[] mainArray, int[] addedArray){
+        int[] result = new int[24];
+        for (int i = 0; i < 24; i++){
+            result[i] = mainArray[i] + addedArray[i];
+        }
+
+        return result;
+    }
+
+    private int[] subtractArrays(int[] storeDraft, int[] proposalsSum){
+        int[] result = new int[24];
+        for (int i = 0; i < 24; i++){
+            result[i] = Math.max(0,storeDraft[i] - proposalsSum[i]);
+        }
+
+        return result;
+    }
+}
+
