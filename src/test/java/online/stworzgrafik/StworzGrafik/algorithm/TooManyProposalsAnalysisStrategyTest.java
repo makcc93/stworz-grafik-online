cab5b03078d615feea689c14632ee99dbf20f7f9
Mateusz Kruk <makoo93@gmail.com>
2026-03-22 23:04:47 +0100

    Add resolve method test

--- src/test/java/online/stworzgrafik/StworzGrafik/algorithm/TooManyProposalsAnalysisStrategyTest.java
+++ src/test/java/online/stworzgrafik/StworzGrafik/algorithm/TooManyProposalsAnalysisStrategyTest.java
@@ -2,32 +2,87 @@ package online.stworzgrafik.StworzGrafik.algorithm;
 
 import online.stworzgrafik.StworzGrafik.algorithm.analyzer.AnalyzeType;
 import online.stworzgrafik.StworzGrafik.algorithm.analyzer.ScheduleAnalyzer;
+import online.stworzgrafik.StworzGrafik.algorithm.analyzer.TooManyProposalsAnalysisResult;
+import online.stworzgrafik.StworzGrafik.algorithm.analyzer.TooManyProposalsAnalysisStrategy;
+import online.stworzgrafik.StworzGrafik.employee.Employee;
+import online.stworzgrafik.StworzGrafik.employee.TestEmployeeBuilder;
+import online.stworzgrafik.StworzGrafik.schedule.Schedule;
+import online.stworzgrafik.StworzGrafik.schedule.message.ScheduleMessageService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDate;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class TooManyProposalsAnalysisStrategyTest {
     @InjectMocks
-    private ScheduleAnalyzer analyzer;
+    private TooManyProposalsAnalysisStrategy strategy;
+
+    @Mock
+    private ScheduleMessageService scheduleMessageService;
 
     @Test
-    void resolve_workingTest(){
-        //given
-        LocalDate date = LocalDate.now();
-        int[] originalDailyDraft = {0,0,0,0,0,0,0,0,2,4,6,6,6,6,7,7,7,7,7,5,0,0,0,0};
-        int[] proposalCount = {0,0,0,0,0,0,0,0,3,3,3,3,3,3,0,0,0,0,0,0,0,0,0,0};
-        ScheduleGeneratorContext context = new TestScheduleGeneratorContext().build();
+    void resolve_reduceProposalOnEmployeeWhoCantOpenStoreWithHighestHours(){
+        // given
+        LocalDate date = LocalDate.of(2024, 5, 20);
+        int targetHour = 8;
 
-        //when
-        analyzer.analyzeAndResolve(context,date, Collections.emptyList(),Collections.emptyList(), AnalyzeType.TOO_MANY_PROPOSALS);
+        Employee emp1 = mock(Employee.class);
+        Employee emp2 = mock(Employee.class);
+        Employee empCantOpenStoreWithHighestHours = mock(Employee.class);
+        Employee emp4 = mock(Employee.class);
+        when(emp1.isCanOpenCloseStore()).thenReturn(true);
+        when(emp2.isCanOpenCloseStore()).thenReturn(false);
+        when(empCantOpenStoreWithHighestHours.isCanOpenCloseStore()).thenReturn(false);
+        when(emp4.isCanOpenCloseStore()).thenReturn(false);
 
-        //then
+        int[] originalDailyDraft = new int[24];
+        originalDailyDraft[targetHour] = 3;
 
+        int[] proposalCount = new int[24];
+        proposalCount[targetHour] = 4;
+
+        TooManyProposalsAnalysisResult result = new TooManyProposalsAnalysisResult(originalDailyDraft, proposalCount);
+
+        ScheduleGeneratorContext context = mock(ScheduleGeneratorContext.class);
+        when(context.getStoreActiveEmployees()).thenReturn(List.of(emp1, emp2,empCantOpenStoreWithHighestHours, emp4));
+
+        Map<Employee, Integer> employeeHours = new HashMap<>();
+        employeeHours.put(emp1, 100);
+        employeeHours.put(emp2, 15);
+        employeeHours.put(empCantOpenStoreWithHighestHours, 95);
+        employeeHours.put(emp4, 35);
+        when(context.getEmployeeHours()).thenReturn(employeeHours);
+
+        int[] emp1Proposal = new int[24]; emp1Proposal[targetHour] = 1;
+        int[] emp2Proposal = new int[24]; emp2Proposal[targetHour] = 1;
+        int[] empCantOpenStoreWithHighestHoursProposal = new int[24]; empCantOpenStoreWithHighestHoursProposal[targetHour] = 1;
+        int[] emp4Proposal = new int[24]; emp4Proposal[targetHour] = 1;
+
+        Map<Employee, int[]> proposalsMap = new HashMap<>();
+        proposalsMap.put(emp1, emp1Proposal);
+        proposalsMap.put(emp2, emp2Proposal);
+        proposalsMap.put(empCantOpenStoreWithHighestHours, empCantOpenStoreWithHighestHoursProposal);
+        proposalsMap.put(emp4, emp4Proposal);
+
+        when(context.getMonthlyEmployeesProposalShiftsByDate()).thenReturn(Map.of(date, proposalsMap));
+
+        // when
+        strategy.resolve(result, context, date);
+
+        // then
+        verify(context).updateEmployeeDailyProposal(eq(empCantOpenStoreWithHighestHours), eq(date), any(int[].class));
+        assertEquals(3, proposalCount[targetHour]);
     }
 
 }
