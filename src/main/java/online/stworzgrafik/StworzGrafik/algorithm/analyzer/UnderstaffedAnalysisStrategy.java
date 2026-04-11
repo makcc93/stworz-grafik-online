865b1f4b8f65fc65d1c320627aeb77e79dec744c
Mateusz Kruk <makoo93@gmail.com>
2026-04-12 00:23:35 +0200

    Add new resolver to too many proposals shifts and too few employees to cover demand draft

--- src/main/java/online/stworzgrafik/StworzGrafik/algorithm/analyzer/UnderstaffedAnalysisStrategy.java
+++ src/main/java/online/stworzgrafik/StworzGrafik/algorithm/analyzer/UnderstaffedAnalysisStrategy.java
@@ -1,6 +1,5 @@
 package online.stworzgrafik.StworzGrafik.algorithm.analyzer;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import online.stworzgrafik.StworzGrafik.algorithm.ScheduleGeneratorContext;
 import online.stworzgrafik.StworzGrafik.employee.Employee;
@@ -11,14 +10,13 @@ import online.stworzgrafik.StworzGrafik.shift.Shift;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.*;
 import java.util.stream.Collectors;
 
 @Slf4j
 @Service
-@RequiredArgsConstructor
 public class UnderstaffedAnalysisStrategy implements ScheduleAnalysisStrategy{
-
     @Override
     public AnalyzeType getSupportedType() {
         return AnalyzeType.UNDERSTAFFED;
@@ -39,33 +37,39 @@ public class UnderstaffedAnalysisStrategy implements ScheduleAnalysisStrategy{
         List<Employee> availableEmployees = ((UnderstaffedAnalysisResult) result).availableEmployees();
         List<Shift> shifts = ((UnderstaffedAnalysisResult) result).shifts();
 
-        while (shifts.size() > availableEmployees.size()) {
-            boolean resolved = cancelProposalDayOffAndAddEmployeeToAvailable(availableEmployees, context, day);
+        Set<Employee> employees = new HashSet<>(context.getMonthlyEmployeesProposalShiftsByDate().getOrDefault(day, new HashMap<>()).keySet());
 
+        int i = 1;
+        while (shifts.size() > availableEmployees.size()) {
+            log.info("          wchodze w resolve understaffed po raz {}", i);
+            boolean resolved = modifyProposalToCoverUnmatchedShift(employees, shifts, context, day);
+            i++;
             if (!resolved) break;
         }
     }
 
-    private boolean cancelProposalDayOffAndAddEmployeeToAvailable(List<Employee> availableEmployees, ScheduleGeneratorContext context, LocalDate day){
-        Map<Employee, Integer> employeeProposalDayOffCount = getEmployeeProposalDayOffCount(context, day, availableEmployees);
+    private boolean modifyProposalToCoverUnmatchedShift(Set<Employee> employees , List<Shift> shifts, ScheduleGeneratorContext context, LocalDate day){
+        Map<Employee, Integer> proposalShiftCount = calculateEmployeeProposalShiftCount(employees, context);
 
-        LinkedHashMap<Employee, Integer> sortedByProposalsCountDesc = employeeProposalDayOffCount.entrySet().stream()
+        Optional<Employee> employeeWithMostProposals = proposalShiftCount.entrySet().stream()
                 .sorted((key1, key2) -> key2.getValue().compareTo(key1.getValue()))
                 .collect(Collectors.toMap(
-                                Map.Entry::getKey,
-                                Map.Entry::getValue,
-                                (keyValue1, keyValue2) -> keyValue1,
-                                LinkedHashMap::new
-                        )
-                );
+                        Map.Entry::getKey,
+                        Map.Entry::getValue,
+                        (k1, k2) -> k1,
+                        HashMap::new
+                ))
+                .keySet().stream()
+                .findFirst();
 
-        Optional<Employee> employeeWithHighestProposalsCount = sortedByProposalsCountDesc.keySet().stream().findFirst();
-        if (employeeWithHighestProposalsCount.isEmpty()) {
+        if (employeeWithMostProposals.isEmpty()) {
+            log.info("Nie można znaleźć pracownika z największa liczbą propozycji zmian");
+
             context.registerMessageOnSchedule(
                     new CreateScheduleMessageDTO(
                             ScheduleMessageType.WARNING,
                             ScheduleMessageCode.NO_AVAILABLE_EMPLOYEE,
-                            "Nie można znaleźć pracownika z największą liczbą propozycji dni wolnych",
+                            "Nie można znaleźć pracownika z największą liczbą propozycji zmian",
                             null,
                             day
                     )
@@ -73,53 +77,67 @@ public class UnderstaffedAnalysisStrategy implements ScheduleAnalysisStrategy{
             return false;
         }
 
+        Employee chosenMostProposalsCountEmployee = employeeWithMostProposals.get();
 
-        Employee chosenEmployee = employeeWithHighestProposalsCount.get();
+        int[] chosenEmployeeProposal = context.getMonthlyEmployeesProposalShiftsByDate().getOrDefault(day, new HashMap<>()).getOrDefault(chosenMostProposalsCountEmployee, new int[24]);
+        Shift chosenEmployeeShift = context.findShiftByArray(chosenEmployeeProposal);
 
-        context.deleteShiftFromSchedule(day, chosenEmployee);
-        context.deleteEmployeeDayOffProposal(day, chosenEmployee);
-        availableEmployees.add(chosenEmployee);
+        Optional<Shift> unmatchedShift = shifts.stream()
+                .max(Comparator.comparingInt(
+                (Shift shift) -> shift.getEndHour().getHour() - shift.getStartHour().getHour()
+        ));
 
-        log.info("Propozycja dnia wolnego dla {} {} na dzień {} została anulowana z powodu zbyt małej liczby dostępnych pracowników. Uzasadnienie: ten pracownik ma najwięcej propozycji dni wolnych.",
-                chosenEmployee.getFirstName(),
-                chosenEmployee.getLastName(),
-                day);
+        if (unmatchedShift.isEmpty()) {
+            log.info("Nie można znaleźć zmiany");
 
-        context.registerMessageOnSchedule(
-                new CreateScheduleMessageDTO(
-                        ScheduleMessageType.INFO,
-                        ScheduleMessageCode.UNDERSTAFFED,
-                        "Propozycja dnia wolnego dla " +
-                                chosenEmployee.getFirstName() +
-                                " " +
-                                chosenEmployee.getLastName() +
-                                " na dzień " + day +
-                                " została anulowana z powodu zbyt małej liczby dostępnych pracowników. Uzasadnienie: ten pracownik ma najwięcej propozycji dni wolnych.",
-                        chosenEmployee.getId(),
-                        day
-                )
-        );
+            context.registerMessageOnSchedule(
+                    new CreateScheduleMessageDTO(
+                            ScheduleMessageType.WARNING,
+                            ScheduleMessageCode.NO_AVAILABLE_SHIFT,
+                            "Nie można znaleźć zmiany",
+                            null,
+                            day
+                    )
+            );
+            return false;
+        }
+
+        Shift joinedShift = joinShifts(context, chosenEmployeeShift, unmatchedShift.get());
+
+        context.updateShiftOnSchedule(day,chosenMostProposalsCountEmployee,joinedShift);
+        context.updateEmployeeDailyProposal(chosenMostProposalsCountEmployee,day,context.shiftAsArray(joinedShift));
+        context.updateEmployeeHours(chosenMostProposalsCountEmployee,chosenEmployeeShift,joinedShift);
+        
+        shifts.remove(unmatchedShift.get());
+        employees.remove(chosenMostProposalsCountEmployee);
 
         return true;
     }
 
-    private static Map<Employee, Integer> getEmployeeProposalDayOffCount(ScheduleGeneratorContext context, LocalDate day, List<Employee> availableEmployees) {
-        Map<Employee, int[]> monthlyEmployeesProposalDayOff = context.getMonthlyEmployeesProposalDayOff();
-        Map<Employee, Integer> employeeProposalDayOffCount = new HashMap<>();
+    private Shift joinShifts(ScheduleGeneratorContext context, Shift firstShift, Shift secondShift){
+        LocalTime startHour = (firstShift.getStartHour().getHour() < secondShift.getStartHour().getHour()) ? firstShift.getStartHour() : secondShift.getStartHour();
+        LocalTime endHour = (firstShift.getEndHour().getHour() > secondShift.getEndHour().getHour()) ? firstShift.getEndHour() : secondShift.getEndHour();
 
-        for (Map.Entry<Employee, int[]> entry : monthlyEmployeesProposalDayOff.entrySet()) {
-            Employee employee = entry.getKey();
-            int[] monthlyProposal = entry.getValue();
+        return context.findShiftByHours(startHour,endHour);
+    }
 
-            if (availableEmployees.contains(employee) || monthlyProposal[day.getDayOfMonth() - 1] == 0 || employee.isWarehouseman()) continue;
+    private Map<Employee, Integer> calculateEmployeeProposalShiftCount(Set<Employee> employees, ScheduleGeneratorContext context){
+        Map<Employee, Integer> dayWithProposalCount = new HashMap<>();
 
-            int proposalsCount = 0;
-            for (int dayValue : monthlyProposal) {
-                proposalsCount += dayValue;
+        for (Employee employee : employees) {
+            int value = 0;
+
+            for (Map.Entry<LocalDate, Map<Employee, int[]>> entry : context.getMonthlyEmployeesProposalShiftsByDate().entrySet()) {
+                Map<Employee, int[]> map = entry.getValue();
+
+                if (map.containsKey(employee) && Arrays.stream(map.getOrDefault(employee,new int[24])).sum() > 0){
+                    value++;
+                }
             }
 
-            employeeProposalDayOffCount.put(employee, proposalsCount);
+            dayWithProposalCount.put(employee,value);
         }
-        return employeeProposalDayOffCount;
+
+        return dayWithProposalCount;
     }
 }
