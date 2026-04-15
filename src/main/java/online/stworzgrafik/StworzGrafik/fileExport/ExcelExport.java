9d574e8d653c3963948521f9288b13cbceba0fc7
Mateusz Kruk <makoo93@gmail.com>
2026-04-15 13:54:52 +0200

    Update file exporter to Excel

--- src/main/java/online/stworzgrafik/StworzGrafik/fileExport/ExcelExport.java
+++ src/main/java/online/stworzgrafik/StworzGrafik/fileExport/ExcelExport.java
@@ -1,5 +1,7 @@
 package online.stworzgrafik.StworzGrafik.fileExport;
 
+import de.focus_shift.jollyday.core.HolidayManager;
+import lombok.RequiredArgsConstructor;
 import online.stworzgrafik.StworzGrafik.algorithm.ScheduleGeneratorContext;
 import online.stworzgrafik.StworzGrafik.employee.Employee;
 import online.stworzgrafik.StworzGrafik.shift.Shift;
@@ -9,14 +11,18 @@ import org.springframework.stereotype.Service;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.time.YearMonth;
-import java.time.format.DateTimeFormatter;
+import java.time.format.TextStyle;
 import java.util.*;
 import java.util.stream.Collectors;
 
 @Service
+@RequiredArgsConstructor
 public class ExcelExport implements ExportFile{
+    private final HolidayManager holidayManager;
 
     @Override
     public byte[] export(ScheduleGeneratorContext context) throws IOException {
@@ -34,10 +40,14 @@ public class ExcelExport implements ExportFile{
                         LinkedHashMap::new));
 
         try (Workbook workbook = new XSSFWorkbook()){
+            int columnIndex = 1;
+            Integer year = context.getYear();
+            Integer month = context.getMonth();
+            YearMonth yearMonth = YearMonth.of(year,month);
+
             Sheet sheet = workbook.createSheet("GRAFIK " + context.getMonth() + "_" + context.getYear());
 
             CellStyle headerStyle = createHeaderStyle(workbook);
-            CellStyle dateStyle = createDateStyle(workbook);
             CellStyle dataStyle = createDataStyle(workbook);
             CellStyle totalStyle = createTotalStyle(workbook);
 
@@ -45,34 +55,41 @@ public class ExcelExport implements ExportFile{
             headerRow.createCell(0).setCellValue("Pracownik");
             headerRow.getCell(0).setCellStyle(headerStyle);
 
-//            for (LocalDate date : yearMonth.getMonth().length()){//finalScheduleSortedByDate.keySet() //todo
-//                Cell cell = headerRow.createCell(columnIndex++);
-//                cell.setCellValue(date.format(DATE_FORMATTER));
-//                cell.setCellStyle(headerStyle);
-//            }
 
-            int columnIndex = 1;
-            Integer year = context.getYear();
-            Integer month = context.getMonth();
-            YearMonth yearMonth = YearMonth.of(year,month);
+            for (int day = 1; day <= yearMonth.lengthOfMonth(); day++){
+                LocalDate date = LocalDate.of(year, month, day);
 
-            for (int day = 1; day <= yearMonth.lengthOfMonth(); day++){//finalScheduleSortedByDate.keySet() //todo
                 Cell cell = headerRow.createCell(columnIndex++);
                 cell.setCellValue(day + " " + yearMonth.getMonth().toString());
-                cell.setCellStyle(headerStyle);
+                cell.setCellStyle(defineBackgroundColor(workbook,headerStyle,cell,date));
             }
 
             Cell totalHoursCell = headerRow.createCell(columnIndex++);
-            totalHoursCell.setCellValue("Suma godzin");
+            totalHoursCell.setCellValue("GODZINY");
             totalHoursCell.setCellStyle(headerStyle);
 
             Cell totalWorkingDaysCell = headerRow.createCell(columnIndex++);
-            totalWorkingDaysCell.setCellValue("Dni pracy");
+            totalWorkingDaysCell.setCellValue("DNI PRACY");
             totalWorkingDaysCell.setCellStyle(headerStyle);
 
             int rowNumber = 1;
+            Row row = sheet.createRow(rowNumber++);
+
+
+            for (int day = 1; day <= yearMonth.lengthOfMonth(); day++) {
+                LocalDate date = LocalDate.of(year, month, day);
+
+                Cell employeeName = row.createCell(day);
+                employeeName.setCellValue(yearMonth.atDay(day).getDayOfWeek().getDisplayName(TextStyle.SHORT_STANDALONE, new Locale("pl", "PL")));
+                employeeName.setCellStyle(defineBackgroundColor(workbook,dataStyle,employeeName,date));
+            }
+
+
+
+
             for (Employee employee : employees){
-                Row row = sheet.createRow(rowNumber++);
+                row = sheet.createRow(rowNumber++);
+                row.setHeightInPoints(24);
 
                 Cell employeeName = row.createCell(0);
                 employeeName.setCellValue(employee.getFirstName() + " " + employee.getLastName());
@@ -85,14 +102,12 @@ public class ExcelExport implements ExportFile{
                     if (!scheduleContainsDate(finalScheduleSortedByDate, date)){
                         Cell cell = row.createCell(cellIndex++);
                         cell.setCellValue("w");
-                        cell.setCellStyle(dataStyle);
+                        cell.setCellStyle(defineBackgroundColor(workbook,dataStyle,cell,date));
                         continue;
                     }
 
                     Map<Employee, Shift> dailyMap = finalScheduleSortedByDate.getOrDefault(date, new HashMap<>());
 
-//                    for (Map.Entry<LocalDate, Map<Employee,Shift>> entry : finalScheduleSortedByDate.entrySet()) {
-//                        Map<Employee, Shift> dailyMap = entry.getValue();
 
                         Shift shift = dailyMap.getOrDefault(employee, context.getDefaultDaysOffShift());
 
@@ -107,7 +122,7 @@ public class ExcelExport implements ExportFile{
                             cell.setCellValue(shift.getStartHour() + "\n" + shift.getEndHour());
                         }
 
-                        cell.setCellStyle(dataStyle);
+                        cell.setCellStyle(defineBackgroundColor(workbook,dataStyle,cell,date));
                         }
 
                 Integer workedHours = employeeHours.getOrDefault(employee, 0);
@@ -121,11 +136,67 @@ public class ExcelExport implements ExportFile{
                 workedDaysCell.setCellStyle(totalStyle);
             }
 
+            rowNumber++;
+
+            for (int i = 0; i < 24; i++) {
+                row = sheet.createRow(rowNumber++); //Row row
+
+                LocalTime hour = LocalTime.of(i, 0);
+                Cell employeeName = row.createCell(0);
+                employeeName.setCellValue(hour + " - " + hour.plusHours(1));
+                employeeName.setCellStyle(dataStyle);
+
+                int cellIndex = 1;
+                for (int day = 1; day <= yearMonth.lengthOfMonth(); day++) {
+                    LocalDate date = LocalDate.of(year, month, day);
+                    int[] dailyShiftsCount = sumOfDailyShiftsAsArray(context,date,employees);
+
+                    Cell cell = row.createCell(cellIndex++);
+                    cell.setCellValue(dailyShiftsCount[i]);
+                    cell.setCellStyle(defineBackgroundColor(workbook,dataStyle,cell,date));
+                }
+            }
+
+            sheet.autoSizeColumn(0);
+
             ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
             workbook.write(byteArrayOutputStream);
 
             return byteArrayOutputStream.toByteArray();
         }
+    }
+
+    private CellStyle defineBackgroundColor(Workbook workbook, CellStyle originalCellStyle, Cell cell, LocalDate date){
+
+        if (date.getDayOfWeek() == DayOfWeek.SATURDAY || date.getDayOfWeek() == DayOfWeek.SUNDAY || holidayManager.isHoliday(date)){
+            CellStyle updatedStyle = workbook.createCellStyle();
+
+            updatedStyle.cloneStyleFrom(originalCellStyle);
+            updatedStyle.setFillForegroundColor(IndexedColors.GREY_25_PERCENT.getIndex());
+            updatedStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+
+            cell.setCellStyle(updatedStyle);
+
+            return updatedStyle;
+        }
+
+        return originalCellStyle;
+    }
+
+    private int[] sumOfDailyShiftsAsArray(ScheduleGeneratorContext context, LocalDate date, Set<Employee> employees){
+        Map<Employee, Shift> dailySchedule = context.getFinalSchedule().getOrDefault(date, new HashMap<>());
+        int[] shiftCount = new int[24];
+        for (int i = 0; i < 24; i++) {
+            for (Employee employee : employees) {
+                Shift shift = dailySchedule.getOrDefault(employee, context.findShiftByArray(new int[24]));
+
+                if (!employee.isWarehouseman() && !shift.equals(context.getDefaultVacationShift())) {
+
+                    shiftCount[i] += context.shiftAsArray(shift)[i];
+                }
+            }
+        }
+        return shiftCount;
     }
 
     private boolean scheduleContainsDate(LinkedHashMap<LocalDate, Map<Employee,Shift>> schedule, LocalDate date){
@@ -144,18 +215,8 @@ public class ExcelExport implements ExportFile{
         font.setBold(false);
         font.setFontHeightInPoints((short) 9);
         style.setFont(font);
-        style.setFillForegroundColor(IndexedColors.GREY_25_PERCENT.getIndex());
-        style.setFillPattern(FillPatternType.SOLID_FOREGROUND);
-        style.setBorderBottom(BorderStyle.THIN);
-        style.setBorderTop(BorderStyle.THIN);
-        style.setBorderLeft(BorderStyle.THIN);
-        style.setBorderRight(BorderStyle.THIN);
-        style.setAlignment(HorizontalAlignment.CENTER);
-        return style;
-    }
-
-    private CellStyle createDateStyle(Workbook workbook) {
-        CellStyle style = workbook.createCellStyle();
+//        style.setFillForegroundColor(IndexedColors.GREY_25_PERCENT.getIndex());
+//        style.setFillPattern(FillPatternType.SOLID_FOREGROUND);
         style.setBorderBottom(BorderStyle.THIN);
         style.setBorderTop(BorderStyle.THIN);
         style.setBorderLeft(BorderStyle.THIN);
@@ -170,7 +231,9 @@ public class ExcelExport implements ExportFile{
         style.setBorderTop(BorderStyle.THIN);
         style.setBorderLeft(BorderStyle.THIN);
         style.setBorderRight(BorderStyle.THIN);
-        style.setAlignment(HorizontalAlignment.LEFT);
+        style.setAlignment(HorizontalAlignment.CENTER);
+        style.setVerticalAlignment(VerticalAlignment.CENTER);
+
         return style;
     }
 
