43ab6ed297164fece66e717adb14e5f80627a0e0
Mateusz Kruk <makoo93@gmail.com>
2026-05-17 22:55:10 +0200

    Add delegation applier to algorithm

---
+++ src/main/java/online/stworzgrafik/StworzGrafik/algorithm/preparation/DelegationApplier.java
@@ -0,0 +1,48 @@
+package online.stworzgrafik.StworzGrafik.algorithm.preparation;
+
+import de.focus_shift.jollyday.core.HolidayManager;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import online.stworzgrafik.StworzGrafik.algorithm.ScheduleGeneratorContext;
+import online.stworzgrafik.StworzGrafik.employee.Employee;
+import online.stworzgrafik.StworzGrafik.shift.Shift;
+import org.springframework.stereotype.Component;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.YearMonth;
+import java.util.List;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class DelegationApplier {
+    private final HolidayManager holidayManager;
+
+    public void applyDelegationToSchedule(ScheduleGeneratorContext context){
+        log.info("WPROWADZENIE DELEGACJI");
+        List<Employee> employeesWithDelegation = context.getStoreActiveEmployees().stream()
+                .filter(context::employeeHasPlannedDelegation)
+                .toList();
+
+        Integer year = context.getYear();
+        Integer month = context.getMonth();
+        YearMonth yearMonth = YearMonth.of(year, month);
+
+        Shift vacationShift = context.getDefaultDelegationShift();
+
+        for (int day = 1; day <= yearMonth.lengthOfMonth(); day++){
+            LocalDate date = LocalDate.of(year, month, day);
+
+            if (holidayManager.isHoliday(date) || date.getDayOfWeek() == DayOfWeek.SATURDAY || date.getDayOfWeek() == DayOfWeek.SUNDAY){
+                continue;
+            }
+
+            for (Employee employee : employeesWithDelegation) {
+                if (context.employeeIsOnDelegation(employee, date)) {
+                    context.registerShiftOnSchedule(date,employee,vacationShift,date.getDayOfWeek());
+                }
+            }
+        }
+    }
+}
