62e1bd36d333fdf0d5a7d2ff3ce96359a7648f2d
Mateusz Kruk <makoo93@gmail.com>
2026-05-29 22:40:49 +0200

    Add controller for holiday manager

---
+++ src/main/java/online/stworzgrafik/StworzGrafik/calendar/controller/HolidayController.java
@@ -0,0 +1,34 @@
+package online.stworzgrafik.StworzGrafik.calendar.controller;
+
+import de.focus_shift.jollyday.core.Holiday;
+import de.focus_shift.jollyday.core.HolidayManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/api/holidays")
+@RequiredArgsConstructor
+@Component
+public class HolidayController {
+    private final HolidayManager holidayManager;
+
+    @GetMapping("/{year}/{month}")
+    public ResponseEntity<List<LocalDate>> getHolidays(@PathVariable Integer year,
+                                                       @PathVariable Integer month){
+        List<LocalDate> holidays = holidayManager.getHolidays(year).stream()
+                .map(Holiday::getDate)
+                .filter(date -> date.getMonthValue() == month)
+                .collect(Collectors.toList());
+
+        return ResponseEntity.ok(holidays);
+    }
+}
