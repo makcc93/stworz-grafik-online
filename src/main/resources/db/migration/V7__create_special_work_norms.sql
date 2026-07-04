c26d9f86ff6eadb6a48bbca400e6ae94d9d93485
Mateusz Kruk <makoo93@gmail.com>
2026-07-04 22:48:04 +0200

    Add flyway special work norms file

---
+++ src/main/resources/db/migration/V7__create_special_work_norms.sql
@@ -0,0 +1,4 @@
+INSERT INTO special_work_norm(name,max_daily_hours,weekly_norm,active) VALUES
+('NIEPEŁNOSPRAWNOŚĆ ST. LEKKI', 8.0,40.0,1),
+('KOBIETA W CIĄŻY', 8.0,40.0,1),
+('NIEPEŁNOSPRAWNOŚĆ ST. ŚREDNI/CIĘŻKI',7.0,35.0,1);
