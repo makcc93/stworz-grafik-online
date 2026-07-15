041c913cdc1cf254695922cf389c0be2d3aa1ee5
Mateusz Kruk <makoo93@gmail.com>
2026-07-15 22:40:55 +0200

    Add missing /api/ in request mapping

--- src/main/java/online/stworzgrafik/StworzGrafik/store/modificationHours/controller/ShiftHourModificationConfigController.java
+++ src/main/java/online/stworzgrafik/StworzGrafik/store/modificationHours/controller/ShiftHourModificationConfigController.java
@@ -12,7 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/stores/{storeId}/shift-hour-config")
+@RequestMapping("/api/stores/{storeId}/shift-hour-config")
 @RequiredArgsConstructor
 class ShiftHourModificationConfigController {
 
